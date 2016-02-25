cde693a75f765949808a0cce7e44de74d19e6904
Van Gamo <vangamo.beta@gmail.com>
2016-02-25 15:34:32 +0100

    Adding content for map-bestfavoritedish screen: maps, actionbar icons, fab icon.

--- app/views/eat/map-bestfavoritedish.ts
+++ app/views/eat/map-bestfavoritedish.ts
@@ -1,9 +1,32 @@
-import observable = require("data/observable");
 import pages = require("ui/page");
+import actionBarModule = require("ui/action-bar");
+import switchModule = require("ui/switch");
+import mapsModule = require("nativescript-google-maps-sdk");
 
 
-// Event handler for Page "loaded" event attached in main-page.xml
-export function pageLoaded(args: observable.EventData) {
-    // Get the event sender
-    var page = <pages.Page>args.object;
+import observable = require("data/observable");
+import frameModule = require("ui/frame");
+import Image = require("ui/image");
+// import vmModule = require("./main-view-model");
+
+
+export function pageLoaded( args: observable.EventData ) {
+    var page = <pages.Page> args.object;
+//vmModule.mainViewModel.set( "someProperty", true );
+//    page.bindingContext = vmModule.mainViewModel;
+/*
+    var obj = new observable.Observable();
+    obj.set("someProperty", false);
+    page.bindingContext = obj;
+*/
+}
+
+export function onMapReady( args: observable.EventData ) {
+
+}
+export function onMarkerSelect( args: observable.EventData ) {
+
+}
+export function onCameraChanged( args: observable.EventData ) {
+
 }
