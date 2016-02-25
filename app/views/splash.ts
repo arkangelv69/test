7a07fbcf3fb946dd917fb27bbc3e027b5362095a
Van Gamo <vangamo.beta@gmail.com>
2016-02-25 15:32:57 +0100

    Adding some animation to progress bar for demo.

--- app/views/splash.ts
+++ app/views/splash.ts
@@ -1,10 +1,18 @@
-import observable = require("data/observable");
 import pages = require("ui/page");
-import frameModule = require("ui/frame");
+import actionBarModule = require("ui/action-bar");
 import progressModule = require("ui/progress");
 
+import observable = require("data/observable");
+import frameModule = require("ui/frame");
 import geolocation = require("nativescript-geolocation");
 
+function wait(milliSeconds) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function(){
+           resolve(milliSeconds);
+        }, milliSeconds);
+    });
+}  /* TODO: Delete me, it's for demo propouses. */
 
 // Event handler for Page "loaded"
 export function pageLoaded(args: observable.EventData) {
@@ -16,10 +24,12 @@ export function pageLoaded(args: observable.EventData) {
         geolocation.enableLocationRequest();
     }
 
-    page.bindingContext = { progress : 15 };
+ 	wait(3000).then(function() {
+    	page.bindingContext = { progress : 55 };
+		});  /* TODO: Delete me, it's for demo propouses. */
 
 
-    var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
+    var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 5000}).
 	then(function(loc) {
 		if (loc) {
 //			model.locations.push(loc);
@@ -30,7 +40,9 @@ export function pageLoaded(args: observable.EventData) {
     		topmost.navigate("views/eat/map-bestfavoritedish");
 		}
 	}, function(e){
-		console.log("Error: " + e.message);
+		console.log("I<3D Error: " + e.message);
+		var topmost = frameModule.topmost();
+    		topmost.navigate("views/eat/map-bestfavoritedish");
 	});
 /*
     var topmost = frameModule.topmost();
